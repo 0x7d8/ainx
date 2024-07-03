4b63607defaa3d8d0218192dfe0fca658f62e804
0x7d8 <rjansengd@gmail.com>
2024-07-03 17:35:25 +0200

    upload files

---
+++ README.md
@@ -0,0 +1,22 @@
+# ainx - Pterodactyl Addon Installer
+
+ainx is a command-line tool used to install pterodactyl addons, usually developed
+using blueprint and meant to be bundled for standalone installations.
+
+## Developing
+
+To Develop on this tool, you need to install all required dependencies
+
+```bash
+git clone https://github.com/0x7d8/ainx.git
+
+cd ainx
+
+# make sure to have nodejs installed already
+npm i -g pnpm
+pnpm i
+pnpm install:dev
+
+# ainx is now globally available
+ainx
+```
---
+++ src/types/manifest.ts
@@ -0,0 +1,50 @@
+import { z } from "zod"
+
+const bpFileTransformer = (s: string) => s.replaceAll('(blueprint)', '/tmp/ainx/addon')
+const panelFileTransformer = (s: string) => s.replaceAll('(panel)', process.cwd())
+
+export const step = z.union([
+	z.object({
+		type: z.literal('copy'),
+
+		source: z.string().transform(bpFileTransformer).transform(panelFileTransformer),
+		destination: z.string().transform(panelFileTransformer)
+	}),
+	z.object({
+		type: z.literal('remove'),
+
+		path: z.string().transform(panelFileTransformer)
+	}),
+	z.object({
+		type: z.literal('replace'),
+
+		file: z.string().transform(panelFileTransformer),
+		search: z.string(),
+		replace: z.string(),
+		newline: z.boolean().optional(),
+		global: z.boolean().optional(),
+		unique: z.boolean().optional()
+	}),
+	z.object({
+		type: z.literal('dashboard-route'),
+
+		after: z.object({
+			path: z.string(),
+			name: z.string(),
+			permission: z.string(),
+			component: z.string()
+		}),
+
+		path: z.string(),
+		name: z.string(),
+		permission: z.string(),
+		component: z.string(),
+		componentPath: z.string()
+	})
+])
+
+export const manifest = z.object({
+	id: z.string(),
+	installation: step.array(),
+	removal: step.array().optional()
+})
